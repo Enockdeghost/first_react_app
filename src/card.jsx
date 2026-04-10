5513163ec63906b19ba2cc57e1c84d6ad6451b49
enockdeghost <enockdeghost@gmail.com>
2026-04-10 14:43:05 +0300

    add image in react app

---
+++ src/card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+    return (
+        <div className="card">
+            <img alt="profile_picture " src="https://images.pexels.com/photos/19233298/pexels-photo-19233298.jpeg"></img>
+            <h2>juniour developer</h2>
+            <p>create webapp and mobile apps</p>
+        </div>
+    )
+}
+export default Card;
