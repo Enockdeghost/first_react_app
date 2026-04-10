2337b42f97e702433e46b08b8f17bbfd4bcc0589
enockdeghost <enockdeghost@gmail.com>
2026-04-10 15:07:53 +0300

    add css and image

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import Header from "./Header.jsx"
-import Footer from "./Footer.jsx"
-import Food from "./Food.jsx"
+import Card from "./card.jsx";
+import Button from "./Button.jsx";
 
 function App() {
 
   return (
     <>
-      <Header/>
-      <Footer/>
-      <Food/>
+      <Card/>
+      <Button/>
     </>
   );
 }
